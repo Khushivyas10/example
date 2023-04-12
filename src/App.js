c330a1dad2da46293caaac4f69cc0b32841cbd04
Khushivyas10 <khushivyas1003@gmail.com>
2023-04-12 18:05:50 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import './App.css'
+import { CircularProgress } from '@mui/material'
+// const axios = require('axios')
+
+
+const url = "http://localhost:3004/Employee/"
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [message, showMessage] = useState(false)
+  const [fetching, setFetching] = useState(true)
+
+  const onClickHandler = () => {
+    showMessage(true)
+  }
+
+  const getUserData = async () => {
+
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    setUserData(jsonData);
+    setFetching(false)
+    console.log(userData)
+  };
+
+  useEffect(() => {
+    setFetching(true)
+    getUserData();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Example-1</h2>
+      {fetching && <CircularProgress />}
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+
+        {userData.map((data, key) => {
+          return (
+            <tbody>
+              <tr>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.age}</td>
+              </tr>
+            </tbody>
+          )
+        })}
+
+      </table>
+
+      <h2>Example-2</h2>
+      <button onClick={onClickHandler}> Click me</button>
+      {message && <h1>Button clicked</h1>}
     </div>
+
   );
 }
-
 export default App;
